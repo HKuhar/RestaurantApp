73902a77f1b05e19aea96dc8796052d2c1facb2a
HKuhar <h_kuhar@yahoo.com>
2017-07-31 21:01:21 +0200

    1.0.5

--- RestaurantApp/Models/FakeOrdersRepository.cs
+++ RestaurantApp/Models/FakeOrdersRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RestaurantApp.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,9 +8,12 @@ namespace RestaurantApp.Models
 {
     public class FakeOrdersRepository : IOrdersRepository
     {
-        private List<Order> orders = new List<Order>();
+        private List<Order> orders = new List<Order>
+        {
+            new Order{OrderId = 1, Name = "Hrvoje Kuhar", Street = "Ozaljska", Number = 73, Time = DateTime.Now, Cart = new Cart{cartItems = new List<CartItem>{ new CartItem { Dish = new Dishes { Name = "Murgh Curry" }, Quantity = 2 } } } }
+        };
 
-        private int nextId = 1;
+        private int nextId = 2;
 
         public void AddOrder(Order o)
         {
